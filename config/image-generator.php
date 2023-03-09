fe5b20722b32707f21290d951ba474989674f7d4
Gaurav <gauravmakhecha@gmail.com>
2023-03-09 13:03:42 +0530

    text

--- config/image-generator.php
+++ config/image-generator.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    // The default width of the image when not specified.
     'width' => 300,
 
+    // The default height of the image when not specified.
     'height' => 300,
 ];
